86d5296a735a4807c403d83f22d4683c85206e5f
muhaiminultasin <tasinmuhaiminul@gmail.com>
2025-06-26 20:57:16 +0600

    the new ssect added in solutions

--- src/components/pages/menservices/First.js
+++ src/components/pages/menservices/First.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Gallery01 from "../../../assets/image/impact.jpg";
 
 function First() {
   return (
@@ -128,23 +129,53 @@ function First() {
             </div>
         </section>
 
-        <section className="cta-three">
-            <div className="cta-three-inner">
-                <div className="container">
-                <div className="row">
-                    <div className="col-lg-12">
-                    <div className="cta-three-content text-center">
-                        <h2 className="section-title text-white">
-                        City Goverment Offers a Wide <br />
-                        Range of Online Services
-                        </h2>
-                        <Link to="/donate" className="btn btn-primary"> Donate </Link>
-                    </div>
-                    </div>
+        <section className="service-section">
+        <div className="container">
+          <div className="row justify-content-between">
+
+            <div className="col-lg-5">
+              <div className="about-image">
+                <div className="about-image-inner img-one">
+                  <img src={Gallery01} className="img-fluid" alt="img-2" />
                 </div>
-                </div>
+              </div>
             </div>
-        </section>
+
+            <div className="col-lg-6">
+              <div className="section-title-box">
+                <div className="section-tagline">Why We're Different</div>
+
+                <h2 className="section-title text-white">
+                  Manufacturing for Scalable Impact
+                </h2>
+                <div className="section-text">
+                  <p>
+                    At the Global Housing Initiative and NeoPOLYGON Technology,
+                    we are establishing our in-house steel fabrication system to
+                    produce all core components of the PolyFRAME structural
+                    system. This includes beams, columns, and connection members
+                    made from 8-gauge steel, utilizing advanced CNC press brakes
+                    and precision cutting technology.
+                  </p>
+                  <p>
+                    Most importantly, the system is fully scalable and can be
+                    installed locally, allowing us to launch projects within
+                    California and across the globe with complete control over
+                    quality, cost, and speed.
+                  </p>
+                  <p>
+                    This capability is central to our vision: delivering resilient, affordable housing through a globally replicable model that starts with smart, localized manufacturing.
+                  </p>
+                </div>
+
+                
+              </div>
+            </div>
+
+            
+          </div>
+        </div>
+      </section>
     </>
   )
 }
